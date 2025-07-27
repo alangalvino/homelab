91ac71c9d354181e8b5ab2e49f25b3dd2e06a67e
Alan Gomes <alangalvino@gmail.com>
2025-07-27 12:58:00 -0300

    Add metallb terraform config using helm and kubernetes providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,4 +95,42 @@ module "talos_cluster" {
   controlplanes    = ["192.168.50.21"]
   workers          = ["192.168.50.22", "192.168.50.23"]
 }
+resource "kubernetes_namespace" "metallb" {
+  metadata {
+    name = "metallb-system"
+
+    labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged"
+    }
+  }
+}
+
+resource "helm_release" "metallb" {
+  name       = "metallb"
+  repository = "https://metallb.github.io/metallb"
+  chart      = "metallb"
+  namespace  = "metallb-system"
+  depends_on = [kubernetes_namespace.metallb]
+}
+
+resource "terraform_data" "metallb_configs" {
+  depends_on = [helm_release.metallb]
+  input      = file("./metallb-config.yaml")
+  provisioner "local-exec" {
+    when        = destroy
+    command     = "echo '${self.input}' | kubectl delete -f -"
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "terraform_data" "apply_metallb_configs" {
+  depends_on = [terraform_data.metallb_configs]
+  lifecycle {
+    replace_triggered_by = [terraform_data.metallb_configs]
+  }
+  provisioner "local-exec" {
+    command     = "echo '${terraform_data.metallb_configs.output}' | kubectl apply -f -"
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
 }
