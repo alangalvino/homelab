351bf2e13d0f6b592f630b35de113b548c5a650b
Alan Gomes <alangalvino@gmail.com>
2025-07-27 12:54:28 -0300

    Create talos-cluster terraform module with extra-mount config to work with Longhorn

---
+++ terraform/modules/talos-cluster/main.tf
@@ -0,0 +1,101 @@
+terraform {
+  required_providers {
+    talos = {
+      source = "siderolabs/talos"
+      version = "0.8.1"
+    }
+  }
+}
+
+resource "talos_machine_secrets" "this" {}
+
+
+data "talos_image_factory_extensions_versions" "this" {
+  talos_version = var.talos_version
+  filters = {
+    names = [
+      "qemu-guest-agent",
+      "iscsi-tools",
+      "util-linux-tools",
+    ]
+  }
+}
+
+resource "talos_image_factory_schematic" "this" {
+  schematic = yamlencode(
+    {
+      customization = {
+        systemExtensions = {
+          officialExtensions = data.talos_image_factory_extensions_versions.this.extensions_info.*.name
+        }
+      }
+    }
+  )
+}
+
+
+data "talos_machine_configuration" "controlplane" {
+  cluster_name     = var.cluster_name
+  cluster_endpoint = "https://${var.cluster_endpoint}:6443"
+  machine_type     = "controlplane"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+data "talos_machine_configuration" "worker" {
+  cluster_name     = var.cluster_name
+  cluster_endpoint = "https://${var.cluster_endpoint}:6443"
+  machine_type     = "worker"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+data "talos_client_configuration" "this" {
+  cluster_name         = var.cluster_name
+  client_configuration = talos_machine_secrets.this.client_configuration
+  endpoints            = [for k in var.controlplanes : k]
+}
+
+resource "talos_machine_configuration_apply" "controlplane" {
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
+  for_each                    = toset(var.controlplanes)
+  node                        = each.key
+  config_patches = [
+    yamlencode({
+      machine = {
+        install = {
+          image =  "factory.talos.dev/nocloud-installer/${talos_image_factory_schematic.this.id}:${var.talos_version}"
+        }
+      }
+    }),
+    file("${path.module}/config-patches/extra-mount.yaml")
+  ]
+}
+
+resource "talos_machine_configuration_apply" "worker" {
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
+  for_each                    = toset(var.workers)
+  node                        = each.key
+  config_patches = [
+    yamlencode({
+      machine = {
+        install = {
+          image =  "factory.talos.dev/nocloud-installer/${talos_image_factory_schematic.this.id}:${var.talos_version}"
+        }
+      }
+    }),
+    file("${path.module}/config-patches/extra-mount.yaml")
+  ]
+}
+
+resource "talos_machine_bootstrap" "this" {
+  depends_on = [talos_machine_configuration_apply.controlplane]
+  client_configuration = talos_machine_secrets.this.client_configuration
+  node                 = [for k in var.controlplanes : k][0]
+}
+
+resource "talos_cluster_kubeconfig" "this" {
+  depends_on           = [talos_machine_bootstrap.this]
+  client_configuration = talos_machine_secrets.this.client_configuration
+  node                 = [for k in var.controlplanes : k][0]
+}
